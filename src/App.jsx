3bbe60b278bbc8cc61807beb63efac691d625ef4
Anmol <anmolawasthi117@gmail.com>
2024-03-31 21:52:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Header from './components/Header'
+import Body from './components/Body'
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Body></Body>
+    </div>
+  )
+}
+
+export default App
