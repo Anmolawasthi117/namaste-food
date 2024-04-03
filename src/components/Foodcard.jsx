af2b581d7cbbe708cb354b8568225edd0c4731cc
Anmol <anmolawasthi117@gmail.com>
2024-04-03 18:58:54 +0530

    first use of useState in filter button

--- src/components/Foodcard.jsx
+++ src/components/Foodcard.jsx
@@ -3,12 +3,14 @@
 import React from 'react'
 
 const Foodcard = (props) => {
+  const {resData} = props;
+  const {img, resName, cousine, rating} = resData;
   return (
     <div className='font-mono p-2 m-5 h-[300px] w-[200px] hover:border  hover:cursor-pointer hover:bg-slate-200 hover:border-solid hover:border-black bg-orange-200 rounded-xl'>
-      <img className=' object-cover h-2/3  rounded-2xl '  src={props.img} alt="" />
-      <h3>{props.resName}</h3>
-      <h3>{props.cousine}</h3>
-      <h3>{props.rating}</h3>
+      <img className=' object-cover h-2/3  rounded-2xl '  src={img} alt="" />
+      <h3>{resName}</h3>
+      <h3>{cousine}</h3>
+      <h3>{rating}</h3>
       <h3>42min</h3>
     </div>
   )
